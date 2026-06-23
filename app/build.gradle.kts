90a5bada271e4ee397c6b4b9bac0b105d4b97649
pirotty2000 <62220863+pirotty2000@users.noreply.github.com>
2026-06-24 06:45:43 +0900

    Ver.1.3.0 UI/UX大幅改善

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 6
-        versionName = "1.3.0"
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
