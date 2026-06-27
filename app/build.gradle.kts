5c329889b357c77f8d5f55f02d518c356841fdbd
pirotty2000 <62220863+pirotty2000@users.noreply.github.com>
2026-06-28 01:19:26 +0900

    Ver.1.6.0 データベース暗号化対応

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 6
-        versionName = "1.5.1"
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
