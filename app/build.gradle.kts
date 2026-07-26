30b6c9fe03dd207d8b9a304487e2ce61fb43e902
pirotty2000 <62220863+pirotty2000@users.noreply.github.com>
2026-07-26 17:37:22 +0900

    Ver.3.0.0-rc-2-fix HealthのUI見直し

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "jp.mydns.fujiwara.carememo"
         minSdk = 31
         targetSdk = 35
-        versionCode = 7 // versionCode = 7 , 2026/07/26 RELEASE Ver.3.0.0-rc-3
-        versionName = "3.0.0-rc-3"
+        versionCode = 8 // versionCode = 8 , 2026/07/26 RELEASE Ver.3.0.0-rc-3-1
+        versionName = "3.0.0-rc-3-1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
