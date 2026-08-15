60fcb67905b326aba5a9e5d9a926c0e07933cc1c
pirotty2000 <62220863+pirotty2000@users.noreply.github.com>
2026-08-15 21:06:42 +0900

    Ver.3.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         targetSdk = 35
         //noinspection HighAppVersionCode
         versionCode = 2026081502
-        versionName = "3.2.7"
+        versionName = "3.2.8"
 
         val buildTime = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.JAPAN).format(Date())
         buildConfigField("String", "BUILD_TIME", "\"$buildTime\"")
